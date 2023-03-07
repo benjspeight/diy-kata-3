ea08757931dfe549ee89601ee8f48a61a1ab9585
Benj <ben.speight@outlook.com>
2023-03-07 21:21:18 +0000

    kata 6 to 8

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,12 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].name === employeeName) {
+          return employees[i].role;
+        }
+      }
+      return "Employee not found";
+
+};
 
 module.exports = getEmployerRole;
